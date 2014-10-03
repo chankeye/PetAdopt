6c4bd6459daef4aebad9ff3a4d929dcbf9da4012
chankeye <chankeye.chen@afusion.com>
2014-10-03 14:22:39 +0800

    Shelters Message

--- Areas/Manage/Controllers/SheltersController.cs
+++ Areas/Manage/Controllers/SheltersController.cs
@@ -36,9 +36,23 @@ namespace PetAdopt.Areas.Manage.Controllers
             return Json(shelterslist);
         }
 
+        public ActionResult GetMessageList(int id, int page, int take)
+        {
+            var result = _sheltersLogic.GetMessageList(id, page, take);
+
+            return Json(result);
+        }
+
         public ActionResult Delete(int id)
         {
             var result = _sheltersLogic.DeleteShelters(Server.MapPath("~/Content/uploads"), id);
+
+            return Json(result);
+        }
+
+        public ActionResult DeleteMessage(int id, int messageId)
+        {
+            var result = _sheltersLogic.DeleteMessage(id, messageId);
 
             return Json(result);
         }

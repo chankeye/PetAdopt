6fc5ea3c653f1ae268769706dc1afb855cfdac95
chankeye <chankeye.chen@afusion.com>
2014-10-13 17:32:41 +0800

    Add front end animal page

---
+++ Areas/Manage/Scripts/App.js
@@ -0,0 +1,50 @@
+﻿window.utils = {
+
+    // 取得地區列表
+    getAreaList: function () {
+        $.ajax({
+            type: 'post',
+            url: '/Manage/System/GetAreaList',
+            success: function (area) {
+                window.vm.areas(area);
+                window.vm.areas.unshift({
+                    "Word": "請選擇",
+                    "Id": ""
+                });
+                $("#selOptionsAreas option:first").attr("selected", true);
+            }
+        });
+    },
+
+    // 取得分類列表
+    getClassList: function () {
+        $.ajax({
+            type: 'post',
+            url: '/Manage/System/GetClassList',
+            success: function (classes) {
+                window.vm.classes(classes);
+                window.vm.classes.unshift({
+                    "Word": "請選擇",
+                    "Id": ""
+                });
+                $("#selOptionsClasses option:first").attr("selected", true);
+            }
+        });
+    },
+
+    // 取得狀態列表
+    getStatusList: function () {
+        $.ajax({
+            type: 'post',
+            url: '/Manage/System/GetStatusList',
+            success: function (statuses) {
+                window.vm.statuses(statuses);
+                window.vm.statuses.unshift({
+                    "Word": "請選擇",
+                    "Id": ""
+                });
+                $("#selOptionsStatuses option:first").attr("selected", true);
+            }
+        });
+    }
+}
---
+++ Controllers/AnimalController.cs
@@ -0,0 +1,89 @@
+﻿using PetAdopt.Controllers;
+using PetAdopt.DTO.Animal;
+using PetAdopt.Logic;
+using System.Web.Mvc;
+
+namespace PetAdopt.Controllers
+{
+    [Authorize]
+    public class AnimalController : _BaseController
+    {
+        #region _animalLogic
+        /// <summary>
+        /// AnimalLogic
+        /// </summary>
+        AnimalLogic _animalLogic
+        {
+            get
+            {
+                if (@animalLogic == null)
+                    @animalLogic = new AnimalLogic(GetOperation());
+                return @animalLogic;
+            }
+        }
+        AnimalLogic @animalLogic;
+        #endregion //_animalLogic
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult GetAnimalList(int page, int take, string query, bool isLike)
+        {
+            var animallist = _animalLogic.GetAnimalList(page, take, query, isLike);
+
+            return Json(animallist);
+        }
+
+        [AllowAnonymous]
+        public ActionResult GetMessageList(int id, int page, int take)
+        {
+            var result = _animalLogic.GetMessageList(id, page, take);
+
+            return Json(result);
+        }
+
+        /*public ActionResult Delete(int id)
+        {
+            var result = _animalLogic.DeleteAnimal(Server.MapPath("~/Content/uploads"), id);
+
+            return Json(result);
+        }
+
+        public ActionResult DeleteMessage(int id, int messageId)
+        {
+            var result = _animalLogic.DeleteMessage(id, messageId);
+
+            return Json(result);
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult EditInit(int id)
+        {
+            var result = _animalLogic.GetAnimal(id);
+
+            return Json(result);
+        }
+
+        public ActionResult AddAnimal(CreateAnimal data)
+        {
+            var result = _animalLogic.AddAnimal(data);
+
+            return Json(result);
+        }
+
+        public ActionResult EditAnimal(int id, CreateAnimal data)
+        {
+            var result = _animalLogic.EditAnimal(id, data);
+
+            return Json(result);
+        }*/
+    }
+}

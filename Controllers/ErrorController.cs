f8dfaa0bcbc75bd8a91d0f04224501e5bf596df9
chankeye <chankeye.chen@afusion.com>
2014-11-12 18:26:26 +0800

    Add error page

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace PetAdopt.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult PageNotFind()
+        {
+            return View();
+        }
+
+        public ActionResult InternalError()
+        {
+            return View();
+        }
+    }
+}
